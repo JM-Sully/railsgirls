5e64ebf09dd2068b8a160808201d1c290077f3a6
Jess Sullivan <jess@scan.com>
2024-10-22 08:51:11 +0100

    Install rubocop, erblint, and upgrade ruby

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/about'
   resources :ideas
-  root to: redirect("/ideas")
+  root to: redirect('/ideas')
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_20_120620) do
-  create_table "ideas", force: :cascade do |t|
-    t.string "name"
-    t.text "description"
-    t.string "picture"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+ActiveRecord::Schema[7.0].define(version: 20_231_120_120_620) do
+  create_table 'ideas', force: :cascade do |t|
+    t.string 'name'
+    t.text 'description'
+    t.string 'picture'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
   end
-
 end
